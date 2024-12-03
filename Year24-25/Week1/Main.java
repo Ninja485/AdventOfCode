e70fce28654633f522642b9f70b5fc1ca512c7bb
Ninja485 <e.gutierrez.2019@alumnos.urjc.es>
2024-12-03 19:25:17 +0100

    Problem day 1 should be done

--- Year24-25/Week1/Main.java
+++ Year24-25/Week1/Main.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> firstArray = new ArrayList<>(Arrays.asList(3,4,2,1,3,3));
+        ArrayList<Integer> secondArray = new ArrayList<>(Arrays.asList(4,3,5,3,9,3));
+        System.out.println(Week1.day1Problem(firstArray, secondArray));
+    }
 }
--- Year24-25/Week1/Week1.java
+++ Year24-25/Week1/Week1.java
@@ -2,15 +2,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Week1 {
-    public int day1Problem(ArrayList<Integer> firstList, ArrayList<Integer> secondList) {
+    public static int day1Problem(ArrayList<Integer> firstList, ArrayList<Integer> secondList) {
         int result = 0;
-        int minFirst,minSecond;
-        for (int i = 0; i < firstList.size(); i++) {
+        Integer minFirst,minSecond;
+        while(!firstList.isEmpty()){
             minFirst = Collections.min(firstList);
             minSecond = Collections.min(secondList);
             result += Math.abs(minFirst - minSecond);
             firstList.remove(minFirst);
+            System.out.println(firstList);
             secondList.remove(minSecond);
+            System.out.println(secondList);
         }
         return result;
     }
